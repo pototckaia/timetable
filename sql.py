fe52e5cd9e006948aaa080fcd92192069564c347
pototckaia <pototckaia@gmail.com>
2017-12-28 14:50:47 +1000

    Modified select

--- sql.py
+++ sql.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 class SqlBuilding:
-
 	def __init__(self, operation, table):
 		self._operation = operation
 		self.fields = []
@@ -20,30 +16,22 @@ class SqlBuilding:
 class BaseQuery(SqlBuilding):
 	def __init__(self, operation, table):
 		super().__init__(operation, table)
-		self.conditions = []
+		self.conditions = None
 		self.sorted_fields = []
 
-	def append_condition(self, cond):
-		self.conditions.append(cond)
-	
 	def append_sort(self, sort):
 		self.sorted_fields.append(sort)
 
 	def clear_param(self):
 		self.fields = []
-		self.conditions = []
+		self.conditions = None
 		self.sorted_fields = []
-		self.logical = None
 
 	def get_table(self):
 		return self.table.name
 
-
-	def add_options(self, fields=[], conditions=[], sorted_fields=[], logical=None):
-		if logical is not None:
-			self.logical = logical
-		for c in conditions:
-			self.conditions.append(c)
+	def add_options(self, fields=[], conditions=None, sorted_fields=[]):
+		self.conditions = conditions if conditions is not None else self.conditions
 		for f in fields:
 			self.fields.append(f)
 		for s in sorted_fields:
@@ -52,21 +40,11 @@ class BaseQuery(SqlBuilding):
 	def get_order_sql(self):
 		if not self.sorted_fields:
 			return ''
-		target = ','.join([' {0}.{1} '.format(field['table'], field['column'])
-						   for field in self.sorted_fields])
+		target = ','.join([' {0}.{1} '.format(field['table'], field['column']) for field in self.sorted_fields])
 		return ' order by ' + target
 
 	def get_where_conditions(self):
-		if self.conditions:
-			if hasattr(self, 'logical') and self.logical is not None:
-				where = 'where ' + self.logical.operation().join([x.get_str() for x in self.conditions])
-			else:
-				where = 'where ' + self.conditions[0].get_str()
-		else:
-			where = ''
-		return where
-
-####
+		return self.conditions.get_query() if self.conditions is not None else ''
 
 class Select(BaseQuery):
 	def __init__(self, table):
@@ -75,11 +53,9 @@ class Select(BaseQuery):
 		self.pagination = False
 
 	def get_left_join_sql(self):
-		return ' '.join([
-				' left join {0} on {0}.{1} = {2}.{3} '.format(
-				left_join['reference_table'], left_join['reference_column'],	
-				self.table.name, left_join['column'
-			]) for left_join in self.left_joins])
+		return ' '.join([' left join {0} on {0}.{1} = {2}.{3} '.format(left_join['reference_table'],
+			left_join['reference_column'], left_join['table'], left_join['column'])
+			for left_join in self.left_joins])
 
 	def get_pagination(self):
 		if self.pagination:
@@ -89,22 +65,20 @@ class Select(BaseQuery):
 
 	def clear_param(self):
 		super().clear_param()
-		self.left_join = []
+		self.left_joins = []
 
 	def append_left_join(self, left_join):
 		self.left_joins.append(left_join)
 
-	def add_options(self, fields=[], conditions=[], sorted_fields=[], logical=None, left_joins=[], pagination=False):
-		super().add_options(fields=fields, conditions=conditions, sorted_fields=sorted_fields, logical=logical)
+	def add_options(self, fields=[], conditions=None, sorted_fields=[], left_joins=[], pagination=False):
+		super().add_options(fields=fields, conditions=conditions, sorted_fields=sorted_fields)
 		for l in left_joins:
 			self.left_joins.append(l)
 		self.pagination = pagination
 
 	def get_column_list(self):
-		return ','.join([
-			' {0}.{1} as a{2} '.format(
-				field['table'], field['column'], str(i)) 
-				for i, field in enumerate(self.fields)])
+		return ','.join([' {0}.{1} as a{2} '.format(
+				field['table'], field['column'], str(i)) for i, field in enumerate(self.fields)])
 		
 	def get_table(self):
 		return ' from {0} '.format(self.table.name)
@@ -126,8 +100,6 @@ class Select(BaseQuery):
 		sort = self.get_order_sql()
 		return super().query() + target + table_from + left_join + where + sort
 
-####
-
 class Update(BaseQuery):
 	def __init__(self, table):
 		super().__init__('update', table)
@@ -141,9 +113,7 @@ class Update(BaseQuery):
 		table_from = self.get_table()
 		where = self.get_where_conditions()
 		sort = self.get_order_sql()
-		return super().query() + table_from + target  + where + sort 
-
-###
+		return super().query() + table_from + target + where + sort
 
 class Delete(BaseQuery):
 	def __init__(self, table):
@@ -156,10 +126,8 @@ class Delete(BaseQuery):
 		table_from = self.get_table()
 		where = self.get_where_conditions()
 		sort = self.get_order_sql()
-		return super().query() + table_from  + where + sort 
+		return super().query() + table_from + where + sort
 
-
-###
 class Insert(SqlBuilding):
 	def __init__(self, table):
 		super().__init__('insert', table)
@@ -171,10 +139,9 @@ class Insert(SqlBuilding):
 		return ' into ' + self.table.name
 
 	def query_values(self):
-		sql =  super().query() + self.get_table() + self.get_column_list() 
+		sql = super().query() + self.get_table() + self.get_column_list()
 		sql += ' values (' + ','.join(['? ' for f in self.fields]) + ') '
 		return sql
 
-	def query_select(self, select_statment):
-		sql = super().query() + self.get_table() + self.get_column_list() + select_statment	
-		return sql
+	def query_select(self, select_statement):
+		return super().query() + self.get_table() + self.get_column_list() + select_statement

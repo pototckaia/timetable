84e65847787dad869282371a59a96c6d5f1805f2
pototckaia <pototckaia@gmail.com>
2017-12-28 14:48:38 +1000

    Add class Conditions

--- condition.py
+++ condition.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 class LogicalOperator:
 	def __init__(self, operation, caption):
 		self._operation = operation
@@ -8,8 +5,7 @@ class LogicalOperator:
 
 	@staticmethod
 	def logical_operations():
-		return sorted(tuple(subclass() for subclass in LogicalOperator.__subclasses__()), 
-			key=lambda x: x.caption)
+		return sorted((subclass() for subclass in LogicalOperator.__subclasses__()), key=lambda x: x.caption)
 
 	@property
 	def caption(self):
@@ -33,8 +29,7 @@ class Compare:
 
 	@staticmethod
 	def comparison_operators():
-		return sorted(tuple(subclass() for subclass in Compare.__subclasses__()), 
-			key=lambda x: x.caption)
+		return sorted(tuple(subclass() for subclass in Compare.__subclasses__()), key=lambda x: x.caption)
 
 	@property
 	def caption(self):
@@ -57,7 +52,7 @@ class Less(Compare):
 
 class Like(Compare):
 	def __init__(self):
-		super().__init__('like', 'Похоже на')
+		super().__init__('like', 'Подстрока')
 
 	def operator(self):
 		return " " + self._operator + "'%' || ? ||  '%'"
@@ -69,3 +64,32 @@ class Condition:
 
 	def get_str(self):
 		return ' {0}.{1} '.format(self.field['table'], self.field['column']) + self.compare_operator.operator()
+
+class Conditions:
+	def __init__(self, columns=[], comparison_number=[], values=[], logical_number=None):
+		list_logical_operators = LogicalOperator.logical_operations()
+		self.logical_operator = list_logical_operators[logical_number] if logical_number is not None else None
+
+		list_comparison_operators = Compare.comparison_operators()
+		comparison_operators = [list_comparison_operators[i] for i in comparison_number]
+
+		self.conditions = []
+		self.values = []
+
+		for i in range(0, len(columns)):
+			self.values.append(columns[i].convert(values[i])) ####
+			self.conditions.append(Condition(columns[i].target, comparison_operators[i]))
+
+
+	def get_query(self):
+		where = ''
+		if self.conditions:
+			if self.logical_operator is not None:
+				where = 'where ' + self.logical_operator.operation().join([x.get_str() for x in self.conditions])
+			else:
+				where = 'where ' + self.conditions[0].get_str()
+		return where
+
+	def append(self, cond, val):
+		self.conditions.append(cond)
+		self.values.append(val)
